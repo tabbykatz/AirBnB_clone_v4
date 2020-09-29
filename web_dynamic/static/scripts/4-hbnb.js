722cdbef3e08efad9b4766ddceee7c905d318148
tabbykatz <tomelay@gmail.com>
2020-09-29 00:19:46 +0000

    4.js, still with 3 error

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
   $('input:checkbox').click(function () {
     $(this).each(function () {
       if (this.checked) {
-	amens[$(this).data('id')] = $(this).data('name');
+        amens[$(this).data('id')] = $(this).data('name');
       } else {
-	delete amens[$(this).data('id')];
+        delete amens[$(this).data('id')];
       }
     });
     if (Object.values(amens).length > 0) {
@@ -25,25 +25,25 @@ $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
   }
 });
 
-const user = {};
+const users = {};
 $.getJSON('http://0.0.0.0:5001/api/v1/users', function (data) {
   for (const user of data) {
-    user[user.id] = user.first_name + ' ' + user.last_name;
+    users[user.id] = user.first_name + ' ' + user.last_name;
   }
 });
 
 $('button').click(function () {
-  console.log("clicked");
+  console.log('clicked');
   $.ajax({
-  type: 'POST',
-  url: 'http://0.0.0.0:5001/api/v1/places_search/',
-  contentType: 'application/json',
-  dataType: 'json',
-  data: '{}',
-  success: function (data) {
-    for (const place of Object.values(data)) {
-      $('section.places').append('<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information">' + '<div class="max_guest">' + '<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div>' + '<div class="number_rooms">' + '<i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div>' + '<div class="number_bathrooms">' + '<i class="fa fa-bath fa-3x" aria-hidden+"true"></i><br />' + place.number_bathrooms + 'Bathrooms</div>' + '</div><div class="user"><strong>Owner: ' + '${users[place.user_id]}</strong></div>' + '<div class="description">' place.description + '</div></article>');
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: '{}',
+    success: function (data) {
+      for (const place of Object.values(data)) {
+        $('section.places').append('<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information">' + '<div class="max_guest">' + '<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div>' + '<div class="number_rooms">' + '<i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div>' + '<div class="number_bathrooms">' + '<i class="fa fa-bath fa-3x" aria-hidden+"true"></i><br />' + place.number_bathrooms + 'Bathrooms</div>' + '</div><div class="user"><strong>Owner: ' + '${users[place.user_id]}</strong></div>' + '<div class="description">' + place.description + '</div></article>');
+      }
     }
-  }
-}  
-}));
+  });
+});
